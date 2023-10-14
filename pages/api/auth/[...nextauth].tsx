cfc71d7f5399ac6fef56a53441a96de05960a75a
Tommy <86784639+tommy88520@users.noreply.github.com>
2023-10-14 10:16:58 +0800

    feat:  auth done

---
+++ pages/api/auth/[...nextauth].tsx
@@ -0,0 +1,66 @@
+import NextAuth from 'next-auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
+
+import { verifyPassword } from '../../../lib/auth';
+import { connectToDatabase } from '../../../lib/db';
+
+export default NextAuth({
+  session: {
+    maxAge: 30 * 24 * 60 * 60, // 30 days
+  },
+  providers: [
+    CredentialsProvider({
+      // The name to display on the sign in form (e.g. "Sign in with...")
+      name: 'Credentials',
+      // `credentials` is used to generate a form on the sign in page.
+      // You can specify which fields should be submitted, by adding keys to the `credentials` object.
+      // e.g. domain, username, password, 2FA token, etc.
+      // You can pass any HTML attribute to the <input> tag through the object.
+      credentials: {
+        username: { label: 'Username', type: 'text', placeholder: 'jsmith' },
+        password: { label: 'Password', type: 'password' },
+      },
+      async authorize(credentials: any): Promise<any> {
+        const client = await connectToDatabase();
+        const usersCollection = client.db().collection('users');
+        const user: any = await usersCollection.findOne({
+          email: credentials.email,
+        });
+        if (!user) {
+          client.close();
+
+          throw new Error('No user found!');
+        }
+        const isValid = await verifyPassword(credentials.password, user.password);
+        if (!isValid) {
+          client.close();
+          return null;
+          // throw new Error('Could not log you in!');
+        }
+        client.close();
+        // return user;
+        return { email: user.email };
+      },
+    }),
+    // CredentialsProvider({
+    // async authorize(credentials: any, req) {
+    //   const client = await connectToDatabase();
+    //   const usersCollection = client.db().collection('users');
+    //   const user = await usersCollection.findOne({
+    //     email: credentials.email,
+    //   });
+    //   if (!user) {
+    //     client.close();
+    //     throw new Error('No user found!');
+    //   }
+    //   const isValid = await verifyPassword(credentials.password, user.password);
+    //   if (!isValid) {
+    //     client.close();
+    //     throw new Error('Could not log you in!');
+    //   }
+    //   client.close();
+    //   return { email: user.email };
+    // },
+    // }),
+  ],
+});
