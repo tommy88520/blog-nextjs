ee9e3993d6e411f0bb68029ec2b8c70ac9cbeedf
Tommy <86784639+tommy88520@users.noreply.github.com>
2023-10-14 10:37:01 +0800

    add env

--- pages/api/auth/[...nextauth].tsx
+++ pages/api/auth/[...nextauth].tsx
@@ -63,6 +63,7 @@ export default NextAuth({
     // },
     // }),
   ],
+  secret: process.env.NEXTAUTH_SECRET,
   callbacks: {
     async jwt({ token, account }) {
       // Persist the OAuth access_token to the token right after signin
